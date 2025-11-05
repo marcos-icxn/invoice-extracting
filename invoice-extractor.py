6eb3946502c3298cd9f13d705887ba9e1e761baa
Marcos <mgual@icxn.com.ar>
2025-11-05 14:00:33 -0300

    add: data_validate function

--- invoice-extractor.py
+++ invoice-extractor.py
@@ -79,38 +79,42 @@ IMPORTANTE:
   "cae": "",
   "observaciones": ""
 }"""
-    
-    response  = client.chat.completions.create(
+
+    response = client.chat.completions.create(
         model="gpt-4o-mini",
         messages=[
-          "role": "user",
-          "content": [
-            {"type": "image_url",
-             "image_url":{
-               "url": f"data:image/jpeg;base64,{base64_image}"
-               "detail": "high"
-             }
+            {
+                "role": "user",
+                "content": [
+                    {
+                        "type": "image_url",
+                        "image_url": {
+                            "url": f"data:image/jpeg;base64,{base64_image}",
+                            "detail": "high",
+                        },
+                    }
+                ],
             }
-          ]
         ],
         response_format={"type": "json-object"},
-        temperature=0
-        max_tokens=1500
+        temperature=0,
+        max_tokens=1500,
     )
-    
+
     json_data = response.choices[0].message.content
     data = json.loads(json_data)
-    
+
     print(f" -> Datos extraídos correctamente...")
-    
+
     return data
-  
-def data_validate(data)
-  errors = []
-  warnings = []
-  
-  if not data.get("numero"):
-    errors.append("Número de factura no encontrado.")
-    
-  if not data.get("proveedor", {}).get("cuit"):
-    errors.append("CUIT del proveedor no encontrado.")
+
+
+def data_validate(data):
+    errors = []
+    warnings = []
+
+    if not data.get("numero"):
+        errors.append("Número de factura no encontrado.")
+
+    if not data.get("proveedor", {}).get("cuit"):
+        errors.append("CUIT del proveedor no encontrado.")
